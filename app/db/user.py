a5a013f08e98a8119db13cad0fab78a21c3f4ba9
lordtachen <matos.aes@gmail.com>
2023-12-18 23:45:30 +0000

    minor improvements

--- app/db/user.py
+++ app/db/user.py
@@ -1,21 +1,21 @@
-from typing import List
+from typing import List,Optional
 
 from sqlalchemy.orm import Session
 from app.models.user import User, Permission
 
 
-def get_user_by_id(db: Session, user_id: int):
+def get_user_by_id(db: Session, user_id: int) -> Optional[User]:
     return db.query(User).filter(User.id == user_id).first()
 
 
-def get_user_by_search_param(db: Session, **filters):
+def get_user_by_search_param(db: Session, **filters) -> List[User]:
     filter_conditions = [
         getattr(User, k).ilike(f"%{v}%") for k, v in filters.items() if v
     ]
     return db.query(User).filter(*filter_conditions).all()
 
 
-def create_user(db: Session, user_data: dict):
+def create_user(db: Session, user_data: dict) -> User:
     db_user = User(**user_data)
     db.add(db_user)
     db.commit()
@@ -23,7 +23,7 @@ def create_user(db: Session, user_data: dict):
     return db_user
 
 
-def update_user(db: Session, user_id: int, user_data: dict):
+def update_user(db: Session, user_id: int, user_data: dict) -> Optional[User]:
     db_user = get_user_by_id(db, user_id)
 
     if db_user:
@@ -36,18 +36,18 @@ def update_user(db: Session, user_id: int, user_data: dict):
     return db_user
 
 
-def delete_user(db: Session, user_id: int):
+def delete_user(db: Session, user_id: int) -> Optional[User]:
     db_user = get_user_by_id(db, user_id)
-
-    if db_user:
-        db.delete(db_user)
-        db.commit()
-        db.refresh(db_user)
-
+    if not db_user:
+        return None
+    db.delete(db_user)
+    db.commit()
+    # ToDO (AM): stil get 500 error when successfully delete
+    print("user after delete",db_user)
     return db_user
 
 
-def create_permission(db: Session, user_id: int, permission_type: str):
+def create_permission(db: Session, user_id: int, permission_type: str) -> Permission:
     db_permission = Permission(user_id=user_id, permission_type=permission_type)
     db.add(db_permission)
     db.commit()
--- app/routes/user.py
+++ app/routes/user.py
@@ -1,16 +1,17 @@
 from typing import List, Optional
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, status, HTTPException
 from fastapi.params import Query
 from sqlalchemy.orm import Session
 from app.db.session import get_db
 from app.db.user import (
+    get_user_by_id,
     get_user_by_search_param,
     create_user,
     update_user,
     delete_user,
 )
 
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 
 router = APIRouter()
 
@@ -18,18 +19,21 @@ router = APIRouter()
 class UserResponse(BaseModel):
     id: int
     name: str
-    email: str
+    email: EmailStr
 
 
 class UserInput(UserResponse):
+    id: int
+    name: str
+    email: EmailStr
     hashed_password: str
 
 
 @router.post("", status_code=status.HTTP_201_CREATED)
-def _create_user(user: UserInput, db: Session = Depends(get_db)) -> UserResponse:
-    user_data = user.model_dump()
-    out_user = create_user(db, user_data).__dict__
-    return UserResponse(**out_user)
+def _create_user(
+    user: UserInput = Depends(), db: Session = Depends(get_db)
+) -> UserResponse:
+    return UserResponse(**create_user(db, user.model_dump()).__dict__)
 
 
 @router.get("")
@@ -49,10 +53,21 @@ def _get_user_by_search_params(
 def _update_user_handler(
     user_id: int, user_update: UserInput, db: Session = Depends(get_db)
 ):
-    user_data = user_update.model_dump(exclude_unset=True)
-    return update_user(db, user_id, user_data)
+    return update_user(db, user_id, user_update.model_dump())
 
 
 @router.delete("/{user_id}", response_model=UserResponse)
 def _delete_user_handler(user_id: int, db: Session = Depends(get_db)):
+    user = delete_user(db, user_id)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
     return delete_user(db, user_id)
+
+@router.get("/{user_id}", response_model=UserResponse)
+def _get_user(
+    user_id: int, db: Session = Depends(get_db)
+):
+    user = get_user_by_id(db,user_id)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return UserResponse( **user.__dict__)
